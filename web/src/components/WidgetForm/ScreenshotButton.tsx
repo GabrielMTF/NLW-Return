a7f01102ee2ad11b84db6ce2e98759aa10e505ee
gabriel.freitas <gabriel.freitas@grupoicts.com.br>
2022-05-09 14:37:53 -0300

    teste

--- web/src/components/WidgetForm/ScreenshotButton.tsx
+++ web/src/components/WidgetForm/ScreenshotButton.tsx
@@ -8,19 +8,19 @@ interface ScreenshotButtonProps {
     onScreenshotTook: (screenshot: string | null) => void;
 }
 
-export function ScreenshotButton({onScreenshotTook, screenshot}: ScreenshotButtonProps) {
+export function ScreenshotButton({ onScreenshotTook, screenshot }: ScreenshotButtonProps) {
 
     const [isTakingScreenshot, setIsTakingScreenshot] = useState(false)
 
-   async function handleTakeScreenshot() {
-    setIsTakingScreenshot(true)
+    async function handleTakeScreenshot() {
+        setIsTakingScreenshot(true)
 
-    const canvas = await html2canvas(document.querySelector('html')!)
-    const base64image = canvas.toDataURL('image/png')
+        const canvas = await html2canvas(document.querySelector('html')!)
+        const base64image = canvas.toDataURL('image/png')
 
-    onScreenshotTook(base64image)
+        onScreenshotTook(base64image)
 
-    setIsTakingScreenshot(false)
+        setIsTakingScreenshot(false)
     }
     if (screenshot) {
         return (
@@ -28,10 +28,10 @@ export function ScreenshotButton({onScreenshotTook, screenshot}: ScreenshotButto
                 backgroundImage: `url(${screenshot})`,
                 backgroundPosition: 'right bottom',
                 backgroundSize: 180,
-            }} 
-            onClick={() => onScreenshotTook(null)}
-            type="button" 
-            className="p-1 w-10 h-10 rounded-md border-transparent flex justify-end items-end text-zinc-400 hover:text-zinc-100 transition-colors" >
+            }}
+                onClick={() => onScreenshotTook(null)}
+                type="button"
+                className="p-1 w-10 h-10 rounded-md border-transparent flex justify-end items-end text-zinc-400 hover:text-zinc-100 transition-colors" >
                 <Trash weight='fill' />
             </button>
         )
@@ -39,11 +39,13 @@ export function ScreenshotButton({onScreenshotTook, screenshot}: ScreenshotButto
 
     return (
         <div>
-            <button onClick={handleTakeScreenshot} type="button" className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500">
+            <button onClick={handleTakeScreenshot}
+                type="button"
+                className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500">
 
-                {isTakingScreenshot ? <Loading/> : <Camera className="w-6 h-6 text-zinc-100" />}
+                {isTakingScreenshot ? <Loading /> : <Camera className="w-6 h-6 text-zinc-100" />}
 
-                
+
             </button>
         </div>
     )
